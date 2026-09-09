b6fea6e90979ede166ac7c654d4d13c06541e4d6
Paolo Francis <paulosensei69@gmail.com>
2026-09-09 14:28:13 +0800

    wire up router, auth/toast/confirm providers, and fix JSX file extensions

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext'
+import { ToastProvider } from './context/ToastContext'
+import { ConfirmProvider } from './context/ConfirmContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <ToastProvider>
+          <ConfirmProvider>
+            <App />
+          </ConfirmProvider>
+        </ToastProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
